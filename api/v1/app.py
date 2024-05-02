06bd94ef7dd4b9e874aaaba6c88a01578e89a174
SepitleFTW <sepitleleshilo69@gmail.com>
2024-05-02 20:00:38 +0000

    ...

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 flask app; registratoin
 """
 from os import getenv
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 
@@ -12,7 +12,22 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 
 
+@app.teardown_appcontext
+def teardown_engine(exception):
+    """exception handling
+    """
+    storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """error 404 message display
+    """
+    response = {"error": "Not Found"}
+    return jsonify(response), 404
+
+
 if __name__ == "__main__":
     HOST = getenv("HBNB_API_HOST", "0.0.0.0")
     PORT = int(getenv("HBNB_API_PORT", 5000))
-    app.run(host=HOST, port=PORT, threaded=True)
+    app.run(debug=True, host=HOST, port=PORT, threaded=True)
