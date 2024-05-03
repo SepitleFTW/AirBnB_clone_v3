0af292e95f48cfe0eb56daaf3164d7230d97d650
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-05-03 19:26:05 +0200

    fixed conflictiong end points

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -34,7 +34,7 @@ def get_city(state_id):
 
 
 @app_views.route('/cities/<city_id>',methods=['DELETE'], strict_slashes=False)
-def get_city(state_id):
+def delete_city(state_id):
     """
     deletes list of cities
     """
