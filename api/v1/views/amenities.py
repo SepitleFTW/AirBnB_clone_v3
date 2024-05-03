75fe821e6e05536a4d975a6ee39588e4961aeb05
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-05-03 23:33:30 +0200

    ...

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -11,11 +11,13 @@ from api.v1.views import app_views
 @app_views.route('/amenities', methods=['GET'], strict_slashes=False)
 def get_all_amenities():
     """Retrieve all amenities"""
-    amenity_list = [amenity.to_dict() for amenity in storage.all(Amenity).values()]
+    amenity_list = [amenity.to_dict() for amenity in
+                    storage.all(Amenity).values()]
     return jsonify(amenity_list)
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['GET'], strict_slashes=False)
+@app_views.route('/amenities/<amenity_id>',
+                 methods=['GET'], strict_slashes=False)
 def get_amenity(amenity_id):
     """Retrieve a specific amenity by ID"""
     amenity = storage.get(Amenity, amenity_id)
@@ -25,7 +27,8 @@ def get_amenity(amenity_id):
         abort(404)
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/amenities/<amenity_id>',
+                 methods=['DELETE'], strict_slashes=False)
 def delete_amenity(amenity_id):
     """Delete an amenity by ID"""
     amenity = storage.get(Amenity, amenity_id)
@@ -53,7 +56,8 @@ def create_amenity():
     return jsonify(amenity.to_dict()), 201
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['PUT'], strict_slashes=False)
+@app_views.route('/amenities/<amenity_id>',
+                 methods=['PUT'], strict_slashes=False)
 def update_amenity(amenity_id):
     """Update an existing amenity"""
     if request.content_type != 'application/json':
