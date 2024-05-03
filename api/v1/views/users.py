0f9028741687078b17f0ea5adbe8260966ec729d
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-05-03 20:13:13 +0200

    fixiing users

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -77,9 +77,9 @@ def update_user(user_id):
     user = storage.get_user(User, user_id)
     if user:
         if not request.get_json():
-            return abort(404, 'Not a JSON')
+            return abort(400, 'Not a JSON')
         if request.content_type != 'application/json':
-            return abort(404, 'Not a JSON')
+            return abort(400, 'Not a JSON')
         data = request.get_json()
 
         ignore_keys = ['id', 'created_at', 'updated_at']
