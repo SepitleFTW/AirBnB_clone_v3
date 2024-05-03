15c1ca1cb3e825031a569a11138ee06a3586264b
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-05-03 21:17:08 +0200

    ...

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,13 +4,17 @@ flask app; registratoin
 """
 from os import getenv
 from flask import Flask, jsonify
+from flask.cors import CORS
 from models import storage
 from api.v1.views import app_views
 
 
 app = Flask(__name__)
 
+CORS(app, resources={r'api/v1/*': {'origins': '0.0.0.0'}})
+
 app.register_blueprint(app_views)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
