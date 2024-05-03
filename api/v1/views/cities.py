a447c34c83ba99a57e4ff470aa231fc0f334aaa0
SepitleFTW <sepitleleshilo69@gmail.com>
2024-05-03 16:50:54 +0000

    ...

---
+++ api/v1/views/cities.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+cities are created
+"""
+from flask import jsonify, abort, request
+from models.state import State
+from models.city import City
+from models import storage
+from api.v1.views import app_views
+
+
+@app_views.route('/states/<state_id>/cities', strict_slashes=False)
+def get_cities_
