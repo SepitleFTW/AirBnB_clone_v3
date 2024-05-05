0cf218059989dcf737fb1b2d577c1216fabe827d
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-05-05 22:47:48 +0200

    ...

--- api/v1/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""new flask blueprint
-and import modules for all the tasks
-"""
-from flask import Blueprint
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-from api.v1.views.index import *
